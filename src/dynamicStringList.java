71935f3a121ea774d85555c2ffb3412b306b4ead
BushyMustache <205280148+BushyMustache@users.noreply.github.com>
2026-04-23 11:34:26 -0700

    finished set method

--- src/dynamicStringList.java
+++ src/dynamicStringList.java
@@ -1,11 +1,22 @@
 public class dynamicStringList implements StringList {
   String[] arr = new String[5];
-
   int sizeCount = 0;
+
   public String get(int index) {
-    if(index < 0 || index >= arr.length) {
+    if (index < 0 || index >= arr.length) {
       throw new IndexOutOfBoundsException();
     }
+
     return arr[index];
   }
+
+  public void set(int index, String value) {
+    if (index < 0 || index >= arr.length) {
+      throw new IndexOutOfBoundsException();
+    }
+
+    arr[index] = value;
+  }
+
+
 }
